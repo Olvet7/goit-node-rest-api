68c239eb4b2bb29d3973b2546f73ddecf74d1399
Olvet7 <volya.maslova@gmail.com>
2024-05-11 17:48:35 +0300

    add morgan

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
-// import morgan from "morgan";
+import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
+
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
